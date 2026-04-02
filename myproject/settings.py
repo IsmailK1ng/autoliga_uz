00fdde781fdc1d18b3de0f51619f1516c1577835
Salohiddin <salohiddinsobirov007@gmail.com>
2026-04-02 10:34:16 +0500

    Settings xatoligi tuzatildi va tekshirildi

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS = str(config('ALLOWED_HOSTS', default='127.0.0.1,localhost')).split(',')
 
 
 

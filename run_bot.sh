f4ee0f8c35ebea42ff4809bde2a207e7db311c78
Salohiddin <salohiddinsobirov007@gmail.com>
2026-04-07 17:07:27 +0500

    koda xato togrlandi

--- run_bot.sh
+++ run_bot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if pgrep -f run_bot.py > /dev/null; then
+    echo "Bot is already running."
+    exit 0
+fi
+
 while true
 do
-  echo "Bot starting at $(date)"
-  bash /home/autolig1/site/Autoliga_Botfile/start_bot.sh
-
-  echo "Bot crashed at $(date). Restarting..."
-  sleep 5
+    echo "Starting bot via run_bot.py at $(date)"
+    python /home/autolig1/site/Autoliga_Botfile/run_bot.py
+    echo "Bot crashed at $(date). Restarting in 5 seconds..."
+    sleep 5
 done
